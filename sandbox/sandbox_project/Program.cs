0f87edd66f1a6b31b7b56b18ade89ac7ca89c7f7
taren-moss <tarendmoss@gmail.com>
2024-03-20 16:40:15 -0700

    W4 Assignment Stuff

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.VisualBasic;
 
 public class Program
 {
@@ -8,44 +9,59 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
+        // I was sure this would work but I really wanted to test
+
+        var set1 = new HashSet<int> {1, 2, 3, 4};
+        var set2 = new HashSet<int> {3, 4, 5, 6};
+        var union = new HashSet<int>();
+        var intersection = new HashSet<int>();
+        var test1 = set1.Union(set2);
+        var test2 = set1.Intersect(set2);
+
+        foreach (var item in set1) {
+            union.Add(item);
+        }
+        foreach (var item in set2) {
+            if (union.Contains(item)) {
+                intersection.Add(item);
+            }
+            else {
+                union.Add(item);
+            }
+        }
+
+        Console.WriteLine("-Manual-");
+        Console.Write("{");
+        foreach (var item in union) {
+            Console.Write($"{item}, ");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+        
+        Console.Write("{");
+        foreach (var item in intersection) {
+            Console.Write($"{item}, ");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+
+        
+        Console.WriteLine();
+        Console.WriteLine("-Built In-");
+        Console.Write("{");
+        foreach (var item in test1) {
+            Console.Write($"{item}, ");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+        
+        Console.Write("{");
+        foreach (var item in test2) {
+            Console.Write($"{item}, ");
+        }
+        Console.Write("}");
+        Console.WriteLine();
+
         Console.WriteLine("Hello Sandbox World!");
     }
 }
-
-// 1
-// 1 2
-// 1 2 3
-// 2 3
-// 3
-// 3 4
-// 3 4 5
-// 4 5
-// 4 5 6
-// 4 5 6 7
-// 4 5 6 7 8
-// 4 5 6 7 8 9
-// 5 6 7 8 9
-// 6 7 8 9
-// 6 7 8 9 10
-// 7 8 9 10
-// 8 9 10
-// 9 10
-// 9 10 11
-// 9 10 11 12
-// 10 11 12
-// 11 12
-// 12
-// 12 13
-// 12 13 14
-// 12 13 14 15
-// 12 13 14 15 16
-// 13 14 15 16
-// 14 15 16
-// 15 16
-// 15 16 17
-// 15 16 17 18
-// 16 17 18
-// 16 17 18 19
-// 16 17 18 19 20
-// 17 18 19 20
-// 18 19 20
--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -21,14 +21,23 @@ public class LinkedList : IEnumerable<int> {
             _head.Prev = newNode; // Connect the previous head to the new node
             _head = newNode; // Update the head to point to the new node
         }
-    }
+    } // I think this function is O(1)
 
     /// <summary>
     /// Insert a new node at the back (i.e. the tail) of the linked list.
     /// </summary>
     public void InsertTail(int value) {
-        // TODO Problem 1
-    }
+        Node newTail = new Node(value);
+        if (_tail is null) { // If there's nothing, the new tail will also be the new head
+            _head = newTail;
+            _tail = newTail;
+        }
+        else { 
+            newTail.Prev = _tail; // The new tail's previous is the current tail
+            _tail.Next = newTail; // The current tail's next if the new tail
+            _tail = newTail; // The new tail officially becomes the current tail
+        }
+    } // I think this function is O(1)
 
 
     /// <summary>
@@ -48,15 +57,22 @@ public class LinkedList : IEnumerable<int> {
             _head.Next!.Prev = null; // Disconnect the second node from the first node
             _head = _head.Next; // Update the head to point to the second node
         }
-    }
+    } // I think this function is O(1)
 
 
     /// <summary>
     /// Remove the last node (i.e. the tail) of the linked list.
     /// </summary>
     public void RemoveTail() {
-        // TODO Problem 2
-    }
+        if (_tail == _head) { // The list will be empty if there's only one thing and it gets deleted
+            _tail = null;
+            _head = null;
+        }
+        else if (_tail is not null) {
+            _tail.Prev!.Next = null; // I suppose this looks at the tail, moves back 1, and then changes the new target's next pointer
+            _tail = _tail.Prev; // I assume without any pointers, the old data is basically invisible and will be overwritten with new data eventually
+        }
+    } // I think this function is O(1)
 
     /// <summary>
     /// Insert 'newValue' after the first occurrence of 'value' in the linked list.
@@ -87,21 +103,48 @@ public class LinkedList : IEnumerable<int> {
 
             curr = curr.Next; // Go to the next node to search for 'value'
         }
-    }
+    } // This function is O(n)
 
     /// <summary>
     /// Remove the first node that contains 'value'.
     /// </summary>
     public void Remove(int value) {
-        // TODO Problem 3
-    }
+        Node? current = _head; // I don't even know if this will work
+        var valueRemoved = false; // This one was tricky. I needed to add this check so it would stop removing information after the first time
+        while (current is not null) {
+            if (current.Data == value) { // If the current value is the target value, do the thing
+                if (valueRemoved == false) {
+                    if (current == _head) {
+                        RemoveHead(); // Remove the head if the current value that is the target value is the head
+                    }
+                    else if (current == _tail) {
+                        RemoveTail(); // Remove the tail if the current value that is the target value is the tail
+                    }
+                    else {
+                        current.Next!.Prev = current.Prev; // I'm attempting to connect the previous node's next pointer to the next node
+                        current.Prev!.Next = current.Next; // And here to connect the next node's previous pointer to the previous node
+                        valueRemoved = true;
+                    }
+                }
+            }
+
+            current = current.Next; // Go to the next node to repeat the check
+        }   
+    } // I believe this function is O(n)
 
     /// <summary>
     /// Search for all instances of 'oldValue' and replace the value to 'newValue'.
     /// </summary>
     public void Replace(int oldValue, int newValue) {
-        // TODO Problem 4
-    }
+        Node? current = _head;
+        while (current is not null) {
+            if (current.Data == oldValue) {
+                current.Data = newValue;
+            }
+
+            current = current.Next;
+        }
+    } // This function is O(n)
 
     /// <summary>
     /// Yields all values in the linked list
@@ -120,15 +163,18 @@ public class LinkedList : IEnumerable<int> {
             yield return curr.Data; // Provide (yield) each item to the user
             curr = curr.Next; // Go forward in the linked list
         }
-    }
+    } // This function is O(n)
 
     /// <summary>
     /// Iterate backward through the Linked List
     /// </summary>
     public IEnumerable Reverse() {
-        // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
-    }
+        var current = _tail;
+        while (current is not null) {
+            yield return current.Data;
+            current = current.Prev;
+        }
+    } // This function is O(n)
 
     public override string ToString() {
         return "<LinkedList>{" + string.Join(", ", this) + "}";
@@ -144,3 +190,5 @@ public class LinkedList : IEnumerable<int> {
         return _head is not null && _tail is not null;
     }
 }
+// All the tests pass so I think that's everything for this assignment? That was surprisingly quick and easy.
+// Maybe too quick and easy. Did I miss or forget something?
